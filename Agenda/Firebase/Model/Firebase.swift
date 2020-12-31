d76ab1d38a5ee99a171bed2e3e464d23ad50bbf3
Fabricio Rodrigo Baixo <fabriciosenna@gmail.com>
2020-12-31 02:13:03 -0300

    Deletando Alunos

--- Agenda/Firebase/Model/Firebase.swift
+++ Agenda/Firebase/Model/Firebase.swift
@@ -11,6 +11,11 @@ import Alamofire
 import Firebase
 
 class Firebase: NSObject {
+    
+    enum StatusDoAluno:Int{
+        case ativo
+        case inativo
+    }
 
     func enviaTokenParaServidor(token:String){
         
@@ -35,7 +40,15 @@ class Firebase: NSObject {
     
     func sincronizaAlunos(alunos:Array<[String:Any]>){
         for aluno in alunos {
-            AlunoDAO().salvaAluno(dicionarioDeAluno: aluno)
+            guard let status = aluno["desativado"] as? Int else {return}
+            if status == StatusDoAluno.ativo.rawValue{
+                AlunoDAO().salvaAluno(dicionarioDeAluno: aluno)
+            }else{
+                guard let idDoAluno = aluno["id"] as? String else { return }
+                guard let aluno = AlunoDAO().recuperaAlunos().filter({$0.id == UUID(uuidString: idDoAluno)}).first else{return}
+                AlunoDAO().deletaAluno(aluno: aluno)
+            }
+            
         }
     }
 }
